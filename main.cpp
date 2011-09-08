8f2f1bc25c376f70a0ae590f191e62a6cc3b8345
Dominik P <ddominikp@o2.pl>
2011-09-08 19:34:16 +0200

    dodana obsługa przez linii komend i dokładniejsze sprawdzanie zmiennej r

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <cmath>
 
 using namespace std;
@@ -6,13 +8,20 @@ using namespace std;
 float r, pole, obw;
 const double PI=3.14159265;
 
-int main(){
-    cout <<"Podaj promien kola: ";
-    cin >> r;
+int main(int argc, char **argv){
+    if(argc==2 && argv[1]>0){
+        r = atof(argv[1]);
+    } else {
+        cout <<"Podaj promien kola: ";
+        if(!(cin >> r)){
+            cout <<"Promien nie jest liczba zmiennoprzecinkowa."<<endl;
+            return 0;
+        }
+    }
     if(r>0){
         pole = PI*pow(r,2.0);
         obw = 2*PI*r;
-        cout <<"\nPole kola o promieniu "<<r<<" wynosi "<<pole<<" zas jego obwod "<<obw<<".";
+        cout <<"Pole kola o promieniu "<<r<<" wynosi "<<pole<<" zas jego obwod "<<obw<<endl;
     } else cout <<"Promien musi byc wiekszy od zera!\n";
     return 0;
 }
