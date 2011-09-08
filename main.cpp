b021aa5f5263b01cbfababc257e0c18e48bcaf26
Dominik P <ddominikp@o2.pl>
2011-09-08 17:31:40 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+float r, pole, obw;
+const double PI=3.14159265;
+
+int main(){
+    cout <<"Podaj promien kola: ";
+    cin >> r;
+    if(r>0){
+        pole = PI*pow(r,2.0);
+        obw = 2*PI*r;
+        cout <<"\nPole kola o promieniu "<<r<<" wynosi "<<pole<<" zas jego obwod "<<obw<<".";
+    } else cout <<"Promien musi byc wiekszy od zera!\n";
+    return 0;
+}
